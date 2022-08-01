4413ce47bdd0d3ae2b84c9f50fdf93fdb42d686b
AbdullahIsha <shahariaresha@gmail.com>
2022-08-01 09:54:34 +0600

    two new page added

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -13,24 +13,24 @@
     <meta name="description" content="Molla - Bootstrap eCommerce Template">
     <meta name="author" content="p-themes">
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/icons/favicon-16x16.png">
-    <link rel="manifest" href="assets/images/icons/site.html">
-    <link rel="mask-icon" href="assets/images/icons/safari-pinned-tab.svg" color="#666666">
-    <link rel="shortcut icon" href="assets/images/icons/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('common/assets/images/icons/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('common/assets/images/icons/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('common/assets/images/icons/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('common/assets/images/icons/site.html')}}">
+    <link rel="mask-icon" href="{{asset('common/assets/images/icons/safari-pinned-tab.svg')}}" color="#666666">
+    <link rel="shortcut icon" href="{{asset('common/assets/images/icons/favicon.ico')}}">
     <meta name="apple-mobile-web-app-title" content="Molla">
     <meta name="application-name" content="Molla">
     <meta name="msapplication-TileColor" content="#cc9966">
-    <meta name="msapplication-config" content="assets/images/icons/browserconfig.xml">
+    <meta name="msapplication-config" content="{{asset('common/assets/images/icons/browserconfig.xml')}}">
     <meta name="theme-color" content="#ffffff">
     <!-- Plugins CSS File -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('common/assets/css/bootstrap.min.css')}}">
     <!-- Main CSS File -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/plugins/owl-carousel/owl.carousel.css">
-    <link rel="stylesheet" href="assets/css/plugins/magnific-popup/magnific-popup.css">
-    <link rel="stylesheet" href="assets/css/plugins/nouislider/nouislider.css">
+    <link rel="stylesheet" href="{{asset('common/assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('common/assets/css/plugins/owl-carousel/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{asset('common/assets/css/plugins/magnific-popup/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('common/assets/css/plugins/nouislider/nouislider.css')}}">
 </head>
 
 <body>
@@ -1881,18 +1881,18 @@
     </div><!-- End .modal -->
 
     <!-- Plugins JS File -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/jquery.hoverIntent.min.js"></script>
-    <script src="assets/js/jquery.waypoints.min.js"></script>
-    <script src="assets/js/superfish.min.js"></script>
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/wNumb.js"></script>
-    <script src="assets/js/bootstrap-input-spinner.js"></script>
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="assets/js/nouislider.min.js"></script>
+    <script src="{{asset('common/assets/js/jquery.min.js')}}"></script>
+    <script src="{{asset('common/assets/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('common/assets/js/jquery.hoverIntent.min.js')}}"></script>
+    <script src="{{asset('common/assets/js/jquery.waypoints.min.js')}}"></script>
+    <script src="{{asset('common/assets/js/superfish.min.js')}}"></script>
+    <script src="{{asset('common/assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('common/assets/js/wNumb.js')}}"></script>
+    <script src="{{asset('common/assets/js/bootstrap-input-spinner.js')}}"></script>
+    <script src="{{asset('common/assets/js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{asset('common/assets/js/nouislider.min.js')}}"></script>
     <!-- Main JS File -->
-    <script src="assets/js/main.js"></script>
+    <script src="{{asset('common/assets/js/main.js')}}"></script>
 </body>
 
 
